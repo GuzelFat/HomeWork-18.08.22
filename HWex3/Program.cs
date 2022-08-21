4d919ab7c325096243bd0f7195e833bdb15000b7
Fatikhova Guzel <nazar.fatikhov@gmail.com>
2022-08-21 10:14:28 +0300

    third  exercise

---
+++ HWex3/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.WriteLine("Введите число: ");
+int A = int.Parse(Console.ReadLine());
+
+if(A%2==0)
+{
+    Console.WriteLine("Четное число");
+}
+else
+{
+    Console.WriteLine("Нечетное число");
+}
