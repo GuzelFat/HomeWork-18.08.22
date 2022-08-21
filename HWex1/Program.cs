eb2298ea7cb37cccec92b6ac0628c23a304157c1
Fatikhova Guzel <nazar.fatikhov@gmail.com>
2022-08-21 10:10:19 +0300

    first exercise

---
+++ HWex1/Program.cs
@@ -0,0 +1,18 @@
+﻿Console.WriteLine("Введите 2 числа");
+int A = int.Parse(Console.ReadLine());
+int B = int.Parse(Console.ReadLine());
+
+if (A>B)
+{
+    Console.Write("max = ");
+    Console.WriteLine(A);
+    Console.Write("min = ");
+    Console.WriteLine(B);
+}
+else 
+{
+    Console.Write("max = ");
+    Console.WriteLine(B);
+    Console.Write("min = ");
+    Console.WriteLine(A);
+}
