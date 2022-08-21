a2cf2060a759cec2987c10f0d20e6c6a19b61bc3
Fatikhova Guzel <nazar.fatikhov@gmail.com>
2022-08-21 10:14:14 +0300

    second exercise

---
+++ HWex2/Program.cs
@@ -0,0 +1,37 @@
+﻿Console.WriteLine("Введите 3 числа");
+int numberA=int.Parse(Console.ReadLine());
+int numberB=int.Parse(Console.ReadLine());
+int numberC=int.Parse(Console.ReadLine());
+int max = 0;
+if (numberA>max) 
+{
+    max=numberA;
+}
+
+else
+{
+    max=max;
+}
+
+if (numberB>max) 
+{
+    max=numberB;
+}
+else
+{
+    max=max;
+}
+if (numberC>max) 
+{
+    max=numberC;
+}
+else
+{
+    max=max;
+}
+Console.Write("max = ");
+Console.WriteLine(max);
+
+
+
+
